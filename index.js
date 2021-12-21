2d11716d5f36ce277ff2e9373843ae7982005b71
Dibyajyoti Mishra <dibyajyotimishraiiit@gmail.com>
2021-12-22 02:08:59 +0530

    promises

---
+++ index.js
@@ -0,0 +1,49 @@
+// Learning Promises
+
+// V1
+
+// const promiseTest = () => {
+//     fetch("https://api2.binance.com/api/v3/ticker/24hr")
+
+//     apiData.then(gotData)
+//     apiData.catch(gotError)
+
+//     function gotData (data) {
+//         console.log(data)
+//     }
+
+//     function gotError(error) {
+//         console.log(error)
+//     }
+// }
+// promiseTest()
+
+
+
+
+// V2
+
+// const promiseTest = () => {
+//     fetch("https://api2.binance.com/api/v3/ticker/24hr").then(gotData).catch(gotError)
+
+//     function gotData (data) {
+//         console.log(data)
+//     }
+
+//     function gotError(error) {
+//         console.log(error)
+//     }
+// }
+// promiseTest()
+
+
+
+// V3
+
+const promiseTest = () => {
+    fetch("https://api2.binance.com/api/v3/ticker/24hr")
+    .then(responce => responce.json())
+    .then(json => console.log(json[1]))
+    .catch(error => console.log(error))
+}
+promiseTest()
